16738d74249339be6e65a838b8f77fce3f062cb6
Carson Crow <carsoncrow@pm.me>
2023-03-27 10:09:01 -0500

    formatted header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import LinkedInIcon from "../assets/linked-in.png";
 import GithubIcon from "../assets/github-mark-white.png";
+import ArrowDown from "../assets/arrow-down.png";
 
 import "../styles/header.css";
 
 const Header = (props) => {
   return (
-    <div className="links-container">
-      <div>
+    <div className="header-container">
+      <div className="link-icon">
         <a
           href="https://www.linkedin.com/in/carson-crow-29b83b254/"
           target="_blank"
@@ -16,7 +17,7 @@ const Header = (props) => {
           <img src={LinkedInIcon} height="30px" width="30px" />
         </a>
       </div>
-      <div>
+      <div className="link-icon">
         <a
           href="https://github.com/shrimpactivity"
           target="_blank"
@@ -24,7 +25,10 @@ const Header = (props) => {
           <img src={GithubIcon} height="30px" width="30px" />
         </a>
       </div>
-      <div><span>Resume &#x2B07;</span></div>
+      <div className="resume-download">
+        <span>Download CV TODO: link file</span>
+        <img src={ArrowDown} height="25px" />
+      </div>
     </div>
   );
 };
