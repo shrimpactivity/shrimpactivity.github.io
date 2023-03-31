2f059872e4bb993d3ba33e9d9366b25f6e698bab
Carson Crow <carsoncrow@pm.me>
2023-03-31 14:02:16 -0500

    added all picks data

--- src/components/menu/content/Picks.js
+++ src/components/menu/content/Picks.js
@@ -1,52 +1,76 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from "react";
 
-import CardContainer from "../cards/CardContainer"
-import Card from '../cards/Card';
-import { books, games, movies } from '../../../data/picks';
-import ShuffleIcon from '../../../assets/icons/shuffle.png';
+import CardContainer from "../cards/CardContainer";
+import Card from "../cards/Card";
+import { books, games, albums } from "../../../data/picks";
+import ShuffleIcon from "../../../assets/icons/shuffle.png";
 
 const chooseRandom = (list) => {
   return list[Math.floor(Math.random() * list.length)];
-}
+};
 
-const Picks = props => {
-  const [book, setBook] = useState(chooseRandom(books));
-  const [game, setGame] = useState(chooseRandom(games));
-  const [movie, setMovie] = useState(chooseRandom(movies));
-
-  const randomizeBook = () => {
-    setBook(chooseRandom(books));
+const shuffle = (list) => {
+  let i, j, x;
+  for (i = 0; i < list.length; i += 1) {
+    j = Math.floor(Math.random() * (list.length - i) + i);
+    x = list[i];
+    list[i] = list[j];
+    list[j] = x;
   }
+  return list;
+};
 
-  const randomizeGame = () => {
-    setGame(chooseRandom(games));
-  }
+const generator = (list) => {
+  const shuffled = shuffle(list);
+  const current = 0;
+  return () => {
+    const result = shuffled[current];
+    current += 1;
+    return result;
+  };
+};
 
-  const randomizeMovie = () => {
-    setMovie(chooseRandom(movies));
-  }
+const Picks = (props) => {
+  const picksRef = useRef({
+    books: shuffle(books),
+    games: shuffle(games),
+    albums: shuffle(albums),
+  });
+
+  const [index, setIndex] = useState(0);
+
+  const refreshPicks = () => {
+    setIndex(index + 1);
+    console.log(picksRef.current.games);
+  };
+
+  const book = picksRef.current.books[index % picksRef.current.books.length];
+  const game = picksRef.current.games[index % picksRef.current.games.length];
+  const album = picksRef.current.albums[index % picksRef.current.albums.length];
 
   return (
     <div className="menu-content">
-      <p style={{textAlign: "center"}}>
-        Allow me to make an entertainment recommendation - below are some things I like!
-      </p>
+      <div className="menu-content-header">
+        <p style={{ textAlign: "center" }}>
+          Bored? Below are a few things I would recommend.
+        </p>
+      </div>
+      <div className="shuffle-button" onClick={refreshPicks}>
+        <img src={ShuffleIcon} alt="shuffle button" />
+      </div>
       <CardContainer>
-        <Card title="Books" image={book.url} imageWidth="60%">
-          <div className="shuffle-button" onClick={randomizeBook}><img src={ShuffleIcon} alt="book cover"/></div>
+        <Card title="Books" image={book.url} imageWidth="50%">
           {book.title}
         </Card>
-        <Card title="Games" image={game.url}>
-          <div className="shuffle-button" onClick={randomizeGame}><img src={ShuffleIcon} alt="video game cover"/></div>
+        <Card title="Games" image={game.url} imageWidth="80%">
           {game.title}
         </Card>
-        <Card title="Movies" image={movie.url} imageWidth="60%">
-          <div className="shuffle-button" onClick={randomizeMovie}><img src={ShuffleIcon} alt="movie cover"/></div>
-          {movie.title}
+        <Card title="Albums" image={album.url} imageWidth="80%">
+          {album.title}
         </Card>
       </CardContainer>
     </div>
-  )
-}
+  );
+};
 
-export default Picks
+export default Picks;
